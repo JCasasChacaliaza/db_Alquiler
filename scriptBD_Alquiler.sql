705665e29a35de2f4e8f72f00d06e5df27455880
James <james.casas@vallegrande.edu.pe>
2018-11-26 09:23:37 -0500

    He crado el scrrip 2

--- scriptBD_Alquiler.sql
+++ scriptBD_Alquiler.sql
@@ -1,6 +1,19 @@
 /*Listar base de datows del servidor */
 show databases;
 
+
 /*Eliminar la base de datos*/
 
-drop database jaja;
+drop database ;
+
+/*Crear ek utf8 para aceptar los caracteres*/
+create database dbAlquiler default character set utf8;
+
+/*poner en uso la basse de datos*/
+
+use dbAlquiler;
+
+/*verificar si esta en uso */
+
+select database ();
+
